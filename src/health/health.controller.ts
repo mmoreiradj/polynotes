d368fce8cc9c08d886394579d4269dcd6b4581b1
martin.moreira-de-jesus <martin.moreira-de-jesus@clicnwork.io>
2023-03-12 20:53:46 +0100

    fix: adapt healthcheck for prisma

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,15 +1,17 @@
 import { Controller, Get } from '@nestjs/common'
-import { HealthCheck, HealthCheckService, MongooseHealthIndicator } from '@nestjs/terminus'
+import { HealthCheck, HealthCheckService } from '@nestjs/terminus'
+import { PrismaService } from 'src/prisma/prisma.service'
 import { Public } from 'src/shared/decorators/is-public.decorator'
+import { PrismaHealthIndicator } from './prisma.check'
 
 @Controller('health')
 export class HealthController {
-  constructor(private health: HealthCheckService, private mongoose: MongooseHealthIndicator) {}
+  constructor(private health: HealthCheckService, private readonly prismaService: PrismaHealthIndicator) {}
 
   @Public()
   @Get()
   @HealthCheck()
   check() {
-    return this.health.check([() => this.mongoose.pingCheck('mongoose')])
+    return this.health.check([() => this.prismaService.isHealthy('db')])
   }
 }
--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,11 +1,13 @@
 import { HttpModule } from '@nestjs/axios'
 import { Module } from '@nestjs/common'
-import { MongooseModule } from '@nestjs/mongoose'
 import { TerminusModule } from '@nestjs/terminus'
+import { PrismaService } from 'src/prisma/prisma.service'
 import { HealthController } from './health.controller'
+import { PrismaHealthIndicator } from './prisma.check'
 
 @Module({
-  imports: [TerminusModule, MongooseModule, HttpModule],
+  imports: [TerminusModule, HttpModule],
+  providers: [PrismaService, PrismaHealthIndicator],
   controllers: [HealthController],
 })
 export class HealthModule {}
