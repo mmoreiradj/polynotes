48df75190742b1c22ed1b2e2ea538c4f915fd060
martin.moreira-de-jesus <martin.moreira-de-jesus@clicnwork.io>
2023-03-14 23:50:46 +0100

    misc: add strong password validation to dto

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,12 +1,20 @@
-import { IsEmail, IsNotEmpty, MaxLength } from 'class-validator'
+import { IsEmail, IsNotEmpty, IsStrongPassword, MaxLength, MinLength } from 'class-validator'
 
 export class CreateUserDto {
   @IsNotEmpty()
   @MaxLength(25)
+  @MinLength(3)
   name: string
 
   @IsNotEmpty()
   @MaxLength(100)
+  @IsStrongPassword({
+    minLength: 12,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
   password: string
 
   @IsNotEmpty()
