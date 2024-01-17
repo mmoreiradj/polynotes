96b1473ed8a89848f6bcd8ada284320216db241a
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2024-01-17 21:14:11 +0100

    fix(*): update dockerfile and prod config

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000/v0'
+export const API_URL = import.meta.env.PROD ? `https://api-${window.location.hostname}` : 'http://localhost:3000/v0'
